3159c91d22860f6a1be1198f69fcd66551327643
AlenaAgeeva <alenapvageeva@gmail.com>
2022-07-03 16:12:41 -0300

    2.1. Преобразование даты [#289476 #318228]

--- src/main/java/utils/HabrCareerDateTimeParser.java
+++ src/main/java/utils/HabrCareerDateTimeParser.java
@@ -4,14 +4,10 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class HabrCareerDateTimeParser implements DateTimeParser {
-    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
 
     @Override
     public LocalDateTime parse(String parse) {
-        String date = parse.split("T")[0]
-                .concat(" ")
-                .concat(parse.split("T")[1]
-                        .split("\\+")[0]);
-        return LocalDateTime.parse(date, formatter);
+        return LocalDateTime.parse(parse, FORMATTER);
     }
 }
