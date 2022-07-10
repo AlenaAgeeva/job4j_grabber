e42959d8d59002891bce38697a78edb146da4915
AlenaAgeeva <alenapvageeva@gmail.com>
2022-07-10 18:58:59 -0300

    7. Web. [#289478 #320705]

--- src/main/java/grabber/Grabber.java
+++ src/main/java/grabber/Grabber.java
@@ -27,6 +27,7 @@ import static org.quartz.TriggerBuilder.newTrigger;
  */
 public class Grabber implements Grab {
     private final Properties cfg = new Properties();
+    private static final String LINK = "https://career.habr.com/vacancies/java_developer?page=";
 
     /**
      * A method creates an object of Store interface via polymorphism.
@@ -84,7 +85,6 @@ public class Grabber implements Grab {
                 .withSchedule(times)
                 .build();
         scheduler.scheduleJob(job, trigger);
-
     }
 
     /**
@@ -103,7 +103,7 @@ public class Grabber implements Grab {
             Store store = (Store) map.get("store");
             Parse parse = (Parse) map.get("parse");
             try {
-                parse.list("https://career.habr.com/vacancies/java_developer?page=")
+                parse.list(LINK)
                         .forEach(store::save);
             } catch (Exception e) {
                 e.printStackTrace();
